ab7daafea7812306e9d9a8654eb2d3e4bea7929d
jinfeng.liu <jinfeng.liu@china.zhaogang.com>
2017-09-15 18:23:48 +0800

    gx

--- wyqp-common/src/main/java/cn/worldwalker/game/wyqp/common/constant/Constant.java
+++ wyqp-common/src/main/java/cn/worldwalker/game/wyqp/common/constant/Constant.java
@@ -1,36 +1,35 @@
 package cn.worldwalker.game.wyqp.common.constant;
 
 import cn.worldwalker.game.wyqp.common.utils.CustomizedPropertyConfigurer;
-import cn.worldwalker.game.wyqp.common.utils.IPUtil;
 
 public class Constant {
 	
 	public final static String curCompany = CustomizedPropertyConfigurer.getContextProperty("cur.company");
 	/**roomId与roomInfo的映射*/
-	public final static String roomIdRoomInfoMap = curCompany + "room_id_room_info_map";
+	public final static String roomIdRoomInfoMap = curCompany + "_room_id_room_info_map";
 	/**roomId与gameType,updateTime的映射*/
-	public final static String roomIdGameTypeUpdateTimeMap = curCompany + "room_id_game_type_update_time_map";
+	public final static String roomIdGameTypeUpdateTimeMap = curCompany + "_room_id_game_type_update_time_map";
 	/**playerId与roomId,gameType的映射*/
-	public final static String playerIdRoomIdGameTypeMap = curCompany + "player_id_room_id_game_type_map";
+	public final static String playerIdRoomIdGameTypeMap = curCompany + "_player_id_room_id_game_type_map";
 	/**offline playerId与roomId,gameType,time的映射关系*/
-	public final static String offlinePlayerIdRoomIdGameTypeTimeMap = curCompany + "offline_player_id_romm_id_game_type_time_map";
+	public final static String offlinePlayerIdRoomIdGameTypeTimeMap = curCompany + "_offline_player_id_romm_id_game_type_time_map";
 	
 	
 	/**ip与此ip上连接数的映射关系*/
-	public final static String ipConnectCountMap = curCompany + "ip_connect_count_map";
+	public final static String ipConnectCountMap = curCompany + "_ip_connect_count_map";
 	/**房卡操作失败数据list*/
-	public final static String roomCardOperationFailList = curCompany + "room_card_operation_fail_list";
+	public final static String roomCardOperationFailList = curCompany + "_room_card_operation_fail_list";
 	
 	/**请求和返回信息日志打印开关*/
-	public final static String logInfoFuse = curCompany + "log_info_fuse";
+	public final static String logInfoFuse = curCompany + "_log_info_fuse";
 	/**登录切换开关*/
-	public final static String loginFuse = curCompany + "login_fuse";
+	public final static String loginFuse = curCompany + "_login_fuse";
 	
 	/**本机ip地址*/
 	public final static String localIp = CustomizedPropertyConfigurer.getContextProperty("local.ip");
 	
 	/**牛牛中，庄类型为抢庄的时候，机器ip与房间id，time的list 映射*/
-	public final static String ipRoomIdTimeMap = curCompany + "ip_room_id_time_map_" + localIp;
+	public final static String ipRoomIdTimeMap = curCompany + "_ip_room_id_time_map_" + localIp;
 	
 
 	public static String noticeMsg = "游戏忠告:文明游戏，禁止赌博及其他违法行为  游戏代理及相关咨询加微信：" + CustomizedPropertyConfigurer.getContextProperty("proxy.cus.weixin");
--- wyqp-common/src/main/java/cn/worldwalker/game/wyqp/common/enums/RoomCardConsumeEnum.java
+++ wyqp-common/src/main/java/cn/worldwalker/game/wyqp/common/enums/RoomCardConsumeEnum.java
@@ -7,15 +7,10 @@ public enum RoomCardConsumeEnum {
 	nn_AAPayTenGames(1, 2, 10, 1, "AA制付费,10局每人1张房卡"),
 	nn_AAPayTwentyGames(1, 2, 20, 2, "AA制付费,20局每人2张房卡"),
 	
-	jh_roomOwnerPayTenGames(3, 1, 10, 4, "房主付费10局需4张房卡"),
-	jh_roomOwnerPayTwentyGames(3, 1, 20, 7, "房主付费20局需7张房卡"),
-	jh_roomOwnerPayThirtyGames(3, 1, 30, 10, "房主付费30局需10张房卡"),
-	jh_AAPayTenGames(3, 2, 10, 1, "AA制付费10局每人需1张房卡"),
-	jh_AAPayTwentyGames(3, 2, 20, 2, "AA制付费20局每人需2张房卡"),
-	jh_AAPayThirtyGames(3, 2, 30, 3, "AA制付费30局每人需3张房卡"),
-	jh_winnerPayTenGames(3, 3, 10, 4, "赢家付费10局需4张房卡"),
-	jh_winnerPayTwentyGames(3, 3, 20, 7, "赢家付费20局需7张房卡"),
-	jh_winnerPayThirtyGames(3, 3, 30, 10, "赢家付费30局需10张房卡");
+	jh_roomOwnerPayTenGames(3, 1, 8, 4, "房主付费8局需4张房卡"),
+	jh_roomOwnerPayTwentyGames(3, 1, 16, 7, "房主付费16局需7张房卡"),
+	jh_AAPayTenGames(3, 2, 8, 1, "AA制付费10局每人需1张房卡"),
+	jh_AAPaySixtyGames(3, 2, 16, 2, "AA制付费16局每人需2张房卡");
 	
 	public Integer gameType;
 	public Integer payType;

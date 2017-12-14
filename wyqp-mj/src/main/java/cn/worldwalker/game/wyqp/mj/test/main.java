c576aabb4311aab6943329632249ad24e41ecb7e
jinfeng.liu <jinfeng.liu@china.zhaogang.com>
2017-12-14 18:07:12 +0800

    gx

---
+++ wyqp-mj/src/main/java/cn/worldwalker/game/wyqp/mj/test/main.java
@@ -0,0 +1,10 @@
+package cn.worldwalker.game.wyqp.mj.test;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t = new Test();
+		t.testOne();
+	}
+
+}
